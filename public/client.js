f87758efe814c9796bb735beb2f9638eac386e3f
Mazahar Shaikh <mazahar.shaikh@wu.com>
2018-05-29 14:32:10 +0530

    made changes for web sockets to make it executable over https

--- public/client.js
+++ public/client.js
@@ -35,7 +35,8 @@ $(function () {
     }
 
     // open connection
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    var HOST = location.origin.replace(/^http/, 'ws').replace(/:9000/,'');
+    var connection = new WebSocket(HOST + ':1337');
 
     connection.onopen = function () {
         // first we want users to enter their names
